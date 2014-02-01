c56d1f490d2b76749042aace85671948355ddbf6
george-gwu <george-gwu@users.noreply.github.com>
2014-02-01 14:02:11 -0500

    Commented out the run loop
    In anticipation of hooking into GUI

--- src/computersimulator/cpu/Computer.java
+++ src/computersimulator/cpu/Computer.java
@@ -12,64 +12,31 @@ public class Computer implements IClockCycle {
     private CentralProcessingUnit cpu;
     private MemoryControlUnit memory;
     private InputOutputController io;
+    
+    // Used to pause until GUI button press between instructions
+    private boolean stepInstruction = false;
+    
+    // Used to pause until GUI button press
+    private boolean stepMicro = false;
 
     public Computer() {        
         memory = new MemoryControlUnit();  
         cpu = new CentralProcessingUnit(memory); // contains ALU,  ControlUnit      
         io = new InputOutputController();
-        
-        // M(15) <- STR, 3, 0, 52, I
-        this.memory.engineerSetMemoryLocation(new Unit(13, 15), Word.WordFromBinaryString("000010 11 00 1 0 00110100"));
-        
-         /* 
-        LDR r, x, address [,I]	
-        which says “load R3 from address 52 indirect with no indexing”  
-        Let location 52 contain 100, and location 100 contain 1023
-        The format in binary looks like this:
-        000001 11 00 1 0 00110100
-        */    
-        // M(16) <- LDR, 3, 0, 52, I
-        this.memory.engineerSetMemoryLocation(new Unit(13, 16), Word.WordFromBinaryString("000001 11 00 1 0 00110100"));
-        
-        /*
-        LDA r, x, address[,I]
-        which says "Load Register with Address"
-        */
-        // M(17) <- LDA, 3, 0, 52, I
-        this.memory.engineerSetMemoryLocation(new Unit(13, 17), Word.WordFromBinaryString("000011 11 00 1 0 00110100"));        
-        
-        /*
-        LDX x, address[,I]
-        which says "Load Index Register from Memory"
-        Let the memory location be 52.
-        */
-        // M(18) <- LDX, 3, 52, I
-        this.memory.engineerSetMemoryLocation(new Unit(13, 18), Word.WordFromBinaryString("101001 00 11 1 0 00110100"));
-               
-        /*
-        STX x, address[,I]
-        which says "Store Index Register to Memory"
-        Let the memory location be 52.
-        */
-        // M(19) <- STX, 3, 52, I
-        this.memory.engineerSetMemoryLocation(new Unit(13, 19), Word.WordFromBinaryString("101010 00 11 1 0 00110100"));
-        
-        // set PC to 15 for testing, this will increment until no more instructions exist, then crash
-        cpu.getControlUnit().setPC(new Unit(13,15));
-        
-        boolean running = true; // @TODO hook this to IPL button
-        
-        do {
-            try {
-                this.clockCycle();
-            } catch(HaltSystemException eHalt){
-                System.out.println("System HALT.");
-                running=false;
-            } catch(Exception e){
-                System.out.println("Error: "+ e);
-            }
-        } while(running==true);
-        
+            
+//        boolean running = true; // @TODO hook this to IPL button
+//        
+//        do {
+//            try {
+//                this.clockCycle();
+//            } catch(HaltSystemException eHalt){
+//                System.out.println("System HALT.");
+//                running=false;
+//            } catch(Exception e){
+//                System.out.println("Error: "+ e);
+//            }
+//        } while(running==true);
+//        
     }   
     
     /**
@@ -82,6 +49,45 @@ public class Computer implements IClockCycle {
             this.cpu.clockCycle();
             this.memory.clockCycle();
             this.io.clockCycle();                
-    }                    
+    }             
+    
+    
+    /*****
+     * 
+// M(15) <- STR, 3, 0, 52, I
+        this.memory.engineerSetMemoryLocation(new Unit(13, 15), Word.WordFromBinaryString("000010 11 00 1 0 00110100"));
+        
+
+//        LDR r, x, address [,I]	
+//        which says “load R3 from address 52 indirect with no indexing”  
+//        Let location 52 contain 100, and location 100 contain 1023
+//        The format in binary looks like this:
+//        000001 11 00 1 0 00110100
+        
+        // M(16) <- LDR, 3, 0, 52, I
+        this.memory.engineerSetMemoryLocation(new Unit(13, 16), Word.WordFromBinaryString("000001 11 00 1 0 00110100"));
+        
+//        LDA r, x, address[,I]
+//        which says "Load Register with Address"
+        // M(17) <- LDA, 3, 0, 52, I
+        this.memory.engineerSetMemoryLocation(new Unit(13, 17), Word.WordFromBinaryString("000011 11 00 1 0 00110100"));        
+        
+//        LDX x, address[,I]
+//        which says "Load Index Register from Memory"
+//        Let the memory location be 52.
+        // M(18) <- LDX, 3, 52, I
+        this.memory.engineerSetMemoryLocation(new Unit(13, 18), Word.WordFromBinaryString("101001 00 11 1 0 00110100"));
+               
+//        STX x, address[,I]
+//        which says "Store Index Register to Memory"
+//        Let the memory location be 52.
+
+        // M(19) <- STX, 3, 52, I
+        this.memory.engineerSetMemoryLocation(new Unit(13, 19), Word.WordFromBinaryString("101010 00 11 1 0 00110100"));
+        
+        // set PC to 15 for testing, this will increment until no more instructions exist, then crash
+        cpu.getControlUnit().setPC(new Unit(13,15));
+* 
+     */
     
 }
