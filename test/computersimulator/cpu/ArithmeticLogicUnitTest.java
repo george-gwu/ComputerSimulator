baedc6be68941f17d2743d7216a50ff2a15f0b70
PawelPerzan <ppp@gwmail.gwu.edu>
2014-02-05 23:46:02 -0500

    add junit test for ArithmeticLogicUnit class

---
+++ test/computersimulator/cpu/ArithmeticLogicUnitTest.java
@@ -0,0 +1,44 @@
+package computersimulator.cpu;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author pawel
+ */
+public class ArithmeticLogicUnitTest {
+    
+    public ArithmeticLogicUnitTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testAdd() {
+        
+    }
+    
+    @Test
+    public void testSubtraction() {
+        
+    }
+}
