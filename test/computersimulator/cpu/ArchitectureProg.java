d132320b2d672fcce3e4e40cef11de2a3dfba35c
PawelPerzan <ppp@gwmail.gwu.edu>
2014-02-08 15:28:33 -0500

    update code for adding two binary numbers.

--- test/computersimulator/cpu/ArchitectureProg.java
+++ test/computersimulator/cpu/ArchitectureProg.java
@@ -24,26 +24,52 @@ public class ArchitectureProg {
 	public String addBinary(String operand1, String operand2) {
 		int carry = 0 ;
 		String res = "";        // result
-		
+                
+                // prepare operands: add leading zeros if needed
+                int diff = Math.abs(operand1.length() - operand2.length());
+                String zeros = "";
+                for (int i = 0; i < diff; i ++) {
+                        zeros +="0";
+                }
+                if (operand1.length() > operand2.length()) {
+                    operand2 = zeros + operand2;
+                } else {
+                    operand1 = zeros + operand1;
+                }
+                
+                
+		// perform addition on operands
 		for (int i = operand2.length() - 1; i >= 0; i--) {
 			int sum = Integer.parseInt((operand1.charAt(i)+"")) + Integer.parseInt((operand2.charAt(i)+"")) + carry;
-                        carry = 0;      // reset carry
+                        carry = 0;              // reset carry
 			res = sum % 2 + res;
 			
 			if (sum >= 2) {
-				carry = 1;
+				carry = 1;   
 			}
 		}
 		
+                // check if overflow occured
+                if (carry == 1)
+                    System.out.print("\noverflow occured. ");
 		return res;
 	}
-
+        
+       
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		ArchitectureProg a = new ArchitectureProg();
-		System.out.println("add: " + a.addBinary("0010",  "0010"));
+                
+                // test cases
+		System.out.println("add operands: " + a.addBinary("0010",  "0010"));
+                
+                System.out.println("add operands: " + a.addBinary("0010",  "10"));
+                
+                System.out.println("add operands: " + a.addBinary("10",  "0000010"));
+                          
+                System.out.println("add operands: " + a.addBinary("10",  "10"));
 
 	}
 }
