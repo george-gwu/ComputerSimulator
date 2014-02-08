41f36ab000394d5a90d54252c2e735645b0897a2
PawelPerzan <ppp@gwmail.gwu.edu>
2014-02-08 14:18:52 -0500

    update code for adding two binary numbers.

--- test/computersimulator/cpu/ArchitectureProg.java
+++ test/computersimulator/cpu/ArchitectureProg.java
@@ -7,7 +7,7 @@ package computersimulator.cpu;
  * 
  */
 public class ArchitectureProg {
-        	/**
+        /**
 	 *  Add two binary numbers
          int carry = 0 ;
    		for ( int i = 0 ; i < N ; i++ )
@@ -23,10 +23,11 @@ public class ArchitectureProg {
 	 */
 	public String addBinary(String operand1, String operand2) {
 		int carry = 0 ;
-		String res = "";								// result
+		String res = "";        // result
 		
 		for (int i = operand2.length() - 1; i >= 0; i--) {
 			int sum = Integer.parseInt((operand1.charAt(i)+"")) + Integer.parseInt((operand2.charAt(i)+"")) + carry;
+                        carry = 0;      // reset carry
 			res = sum % 2 + res;
 			
 			if (sum >= 2) {
@@ -42,7 +43,7 @@ public class ArchitectureProg {
 	 */
 	public static void main(String[] args) {
 		ArchitectureProg a = new ArchitectureProg();
-		System.out.println("add: " + a.addBinary("10",  "10"));
+		System.out.println("add: " + a.addBinary("0010",  "0010"));
 
 	}
 }
