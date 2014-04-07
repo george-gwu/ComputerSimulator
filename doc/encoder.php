769927355ac40d8d8b796ef897d6bd7b7d412a97
George <gschmick@gwu.edu>
2014-04-07 19:32:26 -0400

    Created a string encoder
    This can be used to fit two chars into a single instruction. I plan on
    writing an assembly printstr instruction.

---
+++ doc/encoder.php
@@ -0,0 +1,61 @@
+<?php
+
+$data = array();
+
+$handle = @fopen("strings.txt", "r");
+if ($handle) {
+    while (($buffer = fgets($handle, 4096)) !== false) {
+        $data[] = $buffer;
+    }
+    if (!feof($handle)) {
+        echo "Error: unexpected fgets() fail\n";
+    }
+    fclose($handle);
+}
+
+$outdata = array();
+
+foreach($data as $line){
+	for($i=0; $i<strlen($line);$i++){
+		$char = substr($line, $i, 1);
+		$int = ord($char);
+		$bin = decbin($int);
+		$bin7 = str_pad($bin,7,"0",STR_PAD_LEFT);
+		$outdata[] = compact('char','int','bin','bin7');
+	}
+}
+
+if(count($outdata)%2==1) $outdata[] =array('char'=>'0', 'int'=>0, 'bin'=>'0000000', 'bin7'=>'0000000');
+
+$final = array();
+
+$buffer=array();
+
+$finalC = 0;
+
+do {
+	if(count($buffer)==2){
+		$encoded = str_pad($buffer[0]['bin7'] . $buffer[1]['bin7'], 20, "0", STR_PAD_LEFT);  // encode 2 chars to 20 bytes, little endian		
+		$line = $encoded .'    '. str_pad(strval($finalC), 4, "0", STR_PAD_LEFT) .': '; 
+		$line .= ($buffer[0]['int']>20 ? $buffer[0]['char'] : ' ');
+		$line .= ($buffer[1]['int']>20 ? $buffer[1]['char'] : ' ') .' ';
+		$line .= '\\' . $buffer[0]['int'] . '\\' . $buffer[1]['int'];
+		$final[$finalC++] = $line.PHP_EOL;
+		$buffer=array();
+	}
+
+	$buffer[] = array_shift($outdata);
+
+} while(count($outdata)>0);
+	
+	if(count($buffer)==2){
+		$encoded = str_pad($buffer[0]['bin7'] . $buffer[1]['bin7'], 20, "0", STR_PAD_LEFT);  // encode 2 chars to 20 bytes, little endian		
+		$line = $encoded .'    '. str_pad(strval($finalC), 4, "0", STR_PAD_LEFT) .': '; 
+		$line .= ($buffer[0]['int']>20 ? $buffer[0]['char'] : ' ');
+		$line .= ($buffer[1]['int']>20 ? $buffer[1]['char'] : ' ') .' ';
+		$line .= '\\' . $buffer[0]['int'] . '\\' . $buffer[1]['int'];
+		$final[$finalC++] = $line.PHP_EOL;
+		$buffer=array();
+	}
+
+file_put_contents('output.txt', $final);
