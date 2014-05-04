8042c23ae974b325d7ac27e251f98e153afdd05c
PawelPerzan <pawelperzan@hotmail.com>
2014-05-04 14:47:53 -0400

    stubbing SpeculativeExecutionUnit for speculative execution.

---
+++ src/computersimulator/cpu/SpeculativeExecutionUnit.java
@@ -0,0 +1,35 @@
+package computersimulator.cpu;
+
+/**
+ * Unit for speculative execution
+ *
+ */
+public class SpeculativeExecutionUnit {
+    
+    /**
+     * 
+     * @return true if jump taken
+     */
+    public boolean jumpTaken() {
+        
+        return true;
+        
+    }
+
+    /**
+     * 
+     * @return true if jump not taken
+     */
+    public boolean jumpNotTaken() {
+        return true;
+        
+    }
+    
+    /**
+     * Scans memory
+     */
+    public void ScanMemory() {
+    
+    
+    }
+}
