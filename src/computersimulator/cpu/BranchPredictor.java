3759ee217682f911f1cd80a6cc41ab65ea5dd9e2
George <gschmick@gwu.edu>
2014-05-09 21:54:30 -0400

    Added output for branch prediction

--- src/computersimulator/cpu/BranchPredictor.java
+++ src/computersimulator/cpu/BranchPredictor.java
@@ -4,6 +4,7 @@ import computersimulator.components.MachineFaultException;
 import computersimulator.components.Unit;
 import computersimulator.components.Word;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Unit for speculative execution
@@ -18,12 +19,14 @@ public class BranchPredictor {
     private static final Boolean BRANCH_TAKEN = true;    
 
     private HashMap<Integer, Boolean> branchHistoryTable;
+    private HashMap<Integer, String> branchDescriptorTable;
     
     public BranchPredictor(MemoryControlUnit memory, ControlUnit controlUnit) {
         this.memory = memory;
         this.controlUnit = controlUnit;     
         
         this.branchHistoryTable = new HashMap();
+        this.branchDescriptorTable = new HashMap();
     }
     
     /**
@@ -40,14 +43,29 @@ public class BranchPredictor {
      */
     public void branchNotTaken(int pcRaw) {       
         branchHistoryTable.put(pcRaw, BranchPredictor.BRANCH_NOT_TAKEN);        
-    }    
+    }
+    
+    public void setBranchDescriptor(int pcRaw, String descriptor) {
+        branchDescriptorTable.put(pcRaw, descriptor);
+    }
     
     /**
      * Get the branch prediction table
      * @return
      */
-    public HashMap<Integer, Boolean> getPredictionTable(){
-        return this.branchHistoryTable;
+    public String getPredictionTableForTextArea(){
+        String results="";
+        
+        for (Map.Entry<Integer,String> entry : branchDescriptorTable.entrySet()) {            
+            Integer m = entry.getKey();
+            String descriptor = entry.getValue();
+            Boolean prediction = branchHistoryTable.get(m);            
+            String mZeroPadded = String.format("%04d", m);
+            
+            results += mZeroPadded+"("+descriptor+"): " + ((prediction) ? "Branch Predicted" : "Branch Not Predicted")+"\n";
+        }
+        
+        return results;
     }
 
    
@@ -63,16 +81,25 @@ public class BranchPredictor {
             HashMap<String,Unit> decodedInstruction = controlUnit.decodeInstructionRegister(cell);
             
             Unit opcodeUnit = decodedInstruction.get("opcode");
-            int opcode = opcodeUnit.getUnsignedValue();
+            int opcode = opcodeUnit.getUnsignedValue();           
             
             switch(opcode){
                 case ControlUnit.OPCODE_JCC:
+                    this.setBranchDescriptor(m, "JCC");
+                    this.branchTaken(m);
+                    break;
                 case ControlUnit.OPCODE_JNE:
+                    this.setBranchDescriptor(m, "JNE");
+                    this.branchTaken(m);
+                    break;
                 case ControlUnit.OPCODE_JGE:                
+                    this.setBranchDescriptor(m, "JGE");                  
+                    this.branchTaken(m);
+                    break;
                 case ControlUnit.OPCODE_SOB:
-                    
+                    this.setBranchDescriptor(m, "SOB");             
                     this.branchTaken(m);                                        
-                
+                    break;                
             }
             
         }         
